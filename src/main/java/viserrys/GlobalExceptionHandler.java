d8d1c14b1ec741088b18ca3344a089b5c53c5533
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2024-12-28 17:14:38 +0200

    unz unz

--- src/main/java/viserrys/GlobalExceptionHandler.java
+++ src/main/java/viserrys/GlobalExceptionHandler.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception e, Model model) {
-        // Handle the exception, log it, or perform any necessary actions
-        // For example, you can add an error message to the model
-        // Redirect to an error page or another appropriate page
-        return "redirect:/error";
-    }
+//    @ExceptionHandler(Exception.class)
+//    public String handleException(Exception e, Model model) {
+//        // Handle the exception, log it, or perform any necessary actions
+//        // For example, you can add an error message to the model
+//        // Redirect to an error page or another appropriate page
+//        return "redirect:/error";
+//    }
 }
